eee9592c6c4a5610efbb57f00ed67dd87c250788
Sahid Rahman <sahidrahman404@gmail.com>
2024-08-11 15:12:19 +0700

    add lucia services

--- src/lucia/luciaServices.ts
+++ src/lucia/luciaServices.ts
@@ -3,19 +3,27 @@ import { db } from "@/db/dbServices";
 import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { sessions } from "@/session/sessionSchemas";
 import { users } from "@/user/userSchemas";
+import { Facebook, Google } from "arctic";
+import { config } from "@/config";
+import { TimeSpan } from "oslo";
 
 export const lucia = new Lucia(new DrizzleSQLiteAdapter(db, sessions, users), {
+  sessionExpiresIn: new TimeSpan(2, "w"),
   sessionCookie: {
     attributes: {
       // set to `true` when using HTTPS
       secure: process.env.NODE_ENV === "production",
+      domain: config.server.domain,
     },
   },
   getUserAttributes: (attributes) => {
     return {
       emailVerified: attributes.emailVerified,
+      googleId: attributes.googleId,
+      facebookId: attributes.facebookId,
+      name: attributes.name,
       email: attributes.email,
-      createdAt: attributes.createdAt,
+      createdAt: attributes.createdAt.toJSON(),
     };
   },
 });
@@ -27,11 +35,26 @@ export const hashConfig = {
   parallelism: 1,
 };
 
+export const google = new Google(
+  config.google.clientId!,
+  config.google.clientSecret!,
+  `${config.server.basePath}/v1/oauth/google/callback`,
+);
+
+export const facebook = new Facebook(
+  config.google.clientId!,
+  config.google.clientSecret!,
+  `${config.server.basePath}/v1/oauth/facebook/callback`,
+);
+
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
     DatabaseUserAttributes: {
       email: string;
+      name: string;
+      googleId: string;
+      facebookId: string;
       emailVerified: boolean;
       createdAt: Date;
     };
