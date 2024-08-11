bfdf694fe9b4ae81c6849023ecfa013555b8ff51
Sahid Rahman <sahidrahman404@gmail.com>
2024-08-11 15:06:52 +0700

    update env config

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,24 @@ export const config = {
   server: {
     port: process.env.PORT || 4444,
     basePath: process.env.BASE_PATH || "http://localhost:4444",
+    domain: process.env.DOMAIN || "localhost",
   },
   resend: {
     key: process.env.RESEND_KEY,
+  },
+  email: {
+    from: process.env.EMAIL_FROM || "hello@papiayam.com",
+  },
+  frontend: {
+    basePath: process.env.FRONTEND_BASE_PATH || "http://localhost:3000",
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+  facebook: {
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   },
   db: {
     sqlite: {
