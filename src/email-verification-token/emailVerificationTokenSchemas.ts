428685b82e79484166f4f9938a6745c5d86f81de
Sahid Rahman <sahidrahman404@gmail.com>
2024-08-11 15:12:38 +0700

    update email verification token schema

--- src/email-verification-token/emailVerificationTokenSchemas.ts
+++ src/email-verification-token/emailVerificationTokenSchemas.ts
@@ -1,5 +1,6 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { users } from "@/user/userSchemas";
+import { z } from "zod";
 
 export const emailVerificationToken = sqliteTable("email-verification-tokens", {
   id: text("id").primaryKey(),
@@ -9,3 +10,9 @@ export const emailVerificationToken = sqliteTable("email-verification-tokens", {
   email: text("email").notNull(),
   expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
 });
+
+export const emailVerificationSchema = z.object({
+  email: z.string().email("Invalid email address"),
+});
+
+export type EmailVerification = z.infer<typeof emailVerificationSchema>;
